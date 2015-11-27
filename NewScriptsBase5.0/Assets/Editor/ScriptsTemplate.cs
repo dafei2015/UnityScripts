46d391a245c5abebf0fbd0e20b791fb47995a8f2
dafei2015 <649220234@qq.com>
2015-11-27 11:57:49 +0800

    初始化编辑环境

---
+++ NewScriptsBase5.0/Assets/Editor/ScriptsTemplate.cs
@@ -0,0 +1,32 @@
+﻿#region 描述
+
+// **********************************************************************
+// 
+// 文件名(File Name)：			ScriptsTemplate.cs
+// 
+// 作者(Author)：  				da_fei
+// 
+// 创建时间(CreateTime):			2015-11-27 11:11:30Z
+//
+// 描述(Description):            改变Unity自建脚本的模板，参考游客学院的脚本编写教程             
+//
+// **********************************************************************
+
+#endregion
+
+
+namespace CustomEditor
+{
+    public class ScriptsTemplate : UnityEditor.AssetModificationProcessor
+    {
+        
+        private static void OnWillCreateAsset(string currentPath)
+        {
+            currentPath = currentPath.Replace(".meta", "");
+            if (!currentPath.EndsWith(".cs")) return;
+            string text = System.IO.File.ReadAllText(currentPath);
+            text = text.Replace("#AuthorName#", "da_fei").Replace("#CreateTime#", System.DateTime.Now.ToString("u"));
+            System.IO.File.WriteAllText(currentPath, text);
+        }
+    }
+}
